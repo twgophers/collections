f732aa152db0815b76743e3be83ca887540564a1
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2017-11-12 13:35:54 -0200

    Removing methods from sort api

--- vector.go
+++ vector.go
@@ -7,18 +7,6 @@ import (
 type binaryCondition func(v1, v2 float64) float64
 type Vector []float64
 
-func (v Vector) Len() int {
-	return len(v)
-}
-
-func (v Vector) Less(i, j int) bool {
-	return v[i] < v[j]
-}
-
-func (v Vector) Swap(i, j int) {
-	v[i], v[j] = v[j], v[i]
-}
-
 func (v Vector) Max() float64 {
 	return matchingValue(math.Max, math.Inf(-1), v)
 }
@@ -28,7 +16,7 @@ func (v Vector) Min() float64 {
 }
 
 func (v Vector) Empty() bool {
-	return v.Len() == 0
+	return len(v) == 0
 }
 
 func matchingValue(fn binaryCondition, initial float64, vector Vector) float64 {
